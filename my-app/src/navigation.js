fa1a92db70dc9eb69fd402d8bcaa5d68934ecff8
tanglero4 <tiffanyanglero94@hotmail.com>
2023-04-19 01:50:32 -0400

    Added basic navigation bar code

---
+++ my-app/src/navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NavigationBar() {
+  return (
+    <nav className="navigation-bar vertical-menu hero">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Services</a></li>
+        <li><a href="/">Contact</a></li>
+      </ul>
+    </nav>
+ 
+  );
+}
+
+export default NavigationBar;
