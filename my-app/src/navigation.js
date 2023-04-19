34cb95b68f5abdccf8d6d17d52dd89292b98290c
tanglero4 <tiffanyanglero94@hotmail.com>
2023-04-19 15:22:47 -0400

    Updated navigation styling,added image, title and slogan

--- my-app/src/navigation.js
+++ my-app/src/navigation.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 function NavigationBar() {
   return (
-    <nav className="navigation-bar vertical-menu hero">
+      <section className="hero">
+    <nav>
       <ul>
+         <li className='title'>Justin Time Tree Service</li>
         <li><a href="/">Home</a></li>
         <li><a href="/">About</a></li>
         <li><a href="/">Services</a></li>
         <li><a href="/">Contact</a></li>
       </ul>
     </nav>
- 
+         <section className='slogan'>
+        <p>Experience the Art of Landscape Design and Tree Care</p>
+        </section> 
+      </section>
   );
 }
 
